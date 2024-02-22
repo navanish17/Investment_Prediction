5449093dfc66371dcba23c3ff76999e6e0013769
Navanish Pandey <navnishpande17@gmail.com>
2024-02-22 17:38:35 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('\n', '') for req in requirements]
+
+    return requirements
+
+setup(
+name = 'Investment Prediction',
+version = '1.0.1',
+author = 'Navanish',
+author_email = 'navanishpandey17@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
+
+  
+
+
+
+
