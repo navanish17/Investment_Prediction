cbde229229e5c5a6ae592733bc81a1fc80eb9b70
Navanish Pandey <navnishpande17@gmail.com>
2024-02-23 15:19:18 +0530

    template

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace('\n', '') for req in requirements]
+        requirements = [i.replace('\n', '') for i in requirements]
 
     return requirements
 
